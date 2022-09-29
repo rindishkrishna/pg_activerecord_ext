d2c3e74406acfca2728bdb24a50cbc9ec0922eed
Gautam Punhani <gautamp@thoughtworks.com>
2022-09-29 13:14:44 +0530

    test for the connection failure

--- lib/active_record/connection_adapters/postgres_pipeline_adapter.rb
+++ lib/active_record/connection_adapters/postgres_pipeline_adapter.rb
@@ -5,7 +5,22 @@ module ActiveRecord
   module ConnectionHandling # :nodoc:
     # Establishes a connection to the database that's used by all Active Record objects
     def pipeline_postgresql_connection(config)
-      ConnectionAdapters::PostgresPipelineAdapter.new(nil, nil, nil, config)
+      conn_params = config.symbolize_keys.compact
+
+      # Map ActiveRecords param names to PGs.
+      conn_params[:user] = conn_params.delete(:username) if conn_params[:username]
+      conn_params[:dbname] = conn_params.delete(:database) if conn_params[:database]
+
+      # Forward only valid config params to PG::Connection.connect.
+      valid_conn_param_keys = PG::Connection.conndefaults_hash.keys + [:requiressl]
+      conn_params.slice!(*valid_conn_param_keys)
+
+      ConnectionAdapters::PostgresPipelineAdapter.new(
+        ConnectionAdapters::PostgresPipelineAdapter.new_client(conn_params),
+        logger,
+        conn_params,
+        config,
+        )
     end
   end
 
--- spec/active_record/connection_adapters/postgres_pipeline_adapter_spec.rb
+++ spec/active_record/connection_adapters/postgres_pipeline_adapter_spec.rb
@@ -5,7 +5,8 @@ require 'active_record/connection_adapters/postgres_pipeline_adapter'
 
 RSpec.describe 'ActiveRecord::ConnectionAdapters::PostgresPipelineAdapter' do
   it 'pipeline mode should be on' do
-    connection = ActiveRecord::ConnectionHandling.postgresql_connection(host: File::NULL)
+    #TODO: Create config that connects to the database
+    connection = ActiveRecord::Base.pipeline_postgresql_connection(host: File::NULL)
     raw_conn = connection.instance_variable_get(:@raw_connection)
 
     assert_equal PQ_PIPELINE_ON, raw_conn.pipeline_status
